5e7f56397df9ed2ef2a202f8a8196163a8394d5e
Valerii Stolbov <stolbov.v@outlook.com>
2022-04-27 19:12:54 +0500

    exemles

--- src/components/task-filter/task-filter.jsx
+++ src/components/task-filter/task-filter.jsx
@@ -1,45 +1,45 @@
 import './task-filter.css';
 
 const TaskFilter = (props) => {
-	const { filter, onSelectFilter, onInputTerms } = props;
+  const { filter, onSelectFilter, onInputTerms } = props;
 
-	const buttonData = [
-		{ id: 1, name: 'all' },
-		{ id: 2, name: 'active' },
-		{ id: 3, name: 'completed' },
-	];
+  const buttonData = [
+    { id: 1, name: 'all' },
+    { id: 2, name: 'active' },
+    { id: 3, name: 'completed' },
+  ];
 
-	const buttons = buttonData.map((item) => {
-		const type = item.name === filter.type ? 'current' : 'base';
+  const buttons = buttonData.map((item) => {
+    const type = item.name === filter.type ? 'current' : 'base';
 
-		return (
-			<button
-				key={item.id}
-				className="btn"
-				name={item.name}
-				data-type={type}
-				onClick={(e) => {
-					onSelectFilter(e);
-				}}>
-				{item.name}
-			</button>
-		);
-	});
+    return (
+      <button
+        key={item.id}
+        className="btn"
+        name={item.name}
+        data-type={type}
+        onClick={(e) => {
+          onSelectFilter(e);
+        }}>
+        {item.name}
+      </button>
+    );
+  });
 
-	return (
-		<div className="task-filter">
-			<div className="task-filter__search">
-				<input
-					type="text"
-					value={filter.terms}
-					onInput={(e) => onInputTerms(e)}
-					placeholder="Find a task and/or filter them"
-				/>
-				<i className="bx bx-search"></i>
-			</div>
-			<div className="task-filter__buttons">{buttons}</div>
-		</div>
-	);
+  return (
+    <div className="task-filter">
+      <div className="task-filter__search">
+        <input
+          type="text"
+          value={filter.terms}
+          onInput={(e) => onInputTerms(e)}
+          placeholder="Find a task and/or filter them"
+        />
+        <i className="bx bx-search"></i>
+      </div>
+      <div className="task-filter__buttons">{buttons}</div>
+    </div>
+  );
 };
 
 export default TaskFilter;
